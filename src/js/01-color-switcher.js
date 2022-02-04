200854dd20cd32401afc084554d803a51d8ebaf7
Vladyslav Rozhenko <pochta.vlad@ukr.net>
2022-02-04 22:09:11 +0200

    делает первое задание дз

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+  btnStart: document.querySelector(`[data-start]`),
+  btnStop: document.querySelector(`[data-stop]`),
+};
+let intervalId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+refs.btnStart.addEventListener(`click`, onBtnStartClick);
+refs.btnStop.addEventListener(`click`, onBtnStopClick);
+
+refs.btnStop.disabled = true;
+
+function onBtnStartClick() {
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+}
+function onBtnStopClick() {
+  clearInterval(intervalId);
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+}
